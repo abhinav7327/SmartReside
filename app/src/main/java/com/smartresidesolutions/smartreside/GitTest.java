5e8f29607d1c42ad710f1925eb018eb1991a50f4
udayds <33685812+udayds@users.noreply.github.com>
2018-10-07 12:42:17 +0530

    Added test files for GIT

--- app/src/main/java/com/smartresidesolutions/smartreside/GitTest.java
+++ app/src/main/java/com/smartresidesolutions/smartreside/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_test);
-        //test
+        //test7678
     }
 }
