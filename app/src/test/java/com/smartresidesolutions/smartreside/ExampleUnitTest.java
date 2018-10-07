75e95c9bf3b58d5fb97603983f3ec6288aab7c92
abhinav7327 <abhinav7327@gmail.com>
2018-10-07 13:11:44 +0530

    My commit 3

--- app/src/test/java/com/smartresidesolutions/smartreside/ExampleUnitTest.java
+++ app/src/test/java/com/smartresidesolutions/smartreside/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.smartresidesolutions.smartreside;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
